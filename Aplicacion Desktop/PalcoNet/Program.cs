9e77ee928f08fdea56ba14ff9a87152b35a3f472
Fede <fehc97@hotmail.com>
2018-12-13 00:00:50 -0300

    comiteo el program

--- Aplicacion Desktop/PalcoNet/Program.cs
+++ Aplicacion Desktop/PalcoNet/Program.cs
@@ -16,7 +16,8 @@ namespace PalcoNet
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Abm_Grado.AltaGrado());
+            //Application.Run(new Abm_Grado.AltaGrado());
+            Application.Run(new Listado_Estadistico.ListadoEstadistico());
         }
     }
 }
